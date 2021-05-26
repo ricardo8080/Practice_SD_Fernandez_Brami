40b52a7a171a42b4741f7da0583183f2d0207ef5
Brami01 <bramopit@gmail.com>
2021-05-26 03:32:46 -0400

    changes on index.js publisher

--- TP2/vm1/publish/index.js
+++ TP2/vm1/publish/index.js
@@ -1,16 +1,17 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://brokertp2:7777')
+var client  = mqtt.connect('mqtt://brokertp2:1883')
 
-console.log("im running");
 
-client.on('message', function (topic, message) {
-	console.log(message.toString())
-	setTimeout(
-		function(){
-			client.publish("Hello");
-			console.log("Hello");
-		},
-		5000
-	);
-	client.end();
-});
+function intervalFunc() {
+	console.log('Cant stop me now!');
+	client.publish('presence', 'Hello mqtt')
+  }
+
+client.on('connect', function () {
+	client.subscribe('presence', function (err) {
+		if (!err) {
+			client.publish('presence', 'Hello mqtt')
+			setInterval(intervalFunc, 1500);
+	  }
+	})
+  })

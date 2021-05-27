ffa567757fb8e7458b1b82ca4edd30ad0503e046
kirito319 <yerisoruco@gmail.com>
2021-05-26 20:21:33 -0400

    little changes

--- TP2/vm1/publish/index.js
+++ TP2/vm1/publish/index.js
@@ -1,17 +1,15 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://brokertp2:1883')
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
+//var client  = mqtt.connect('mqtt://brokertp2')
+//var client  = mqtt.connect('mqtt://'+process.env.IP + ':'+process.env.LISTEN)
 
 
 function intervalFunc() {
-	console.log('Cant stop me now!');
-	client.publish('presence', 'Hello mqtt')
-  }
+    console.log('Cant stop me now!');
+    client.publish('presence', 'Hello mqtt')
+}
 
 client.on('connect', function () {
-	client.subscribe('presence', function (err) {
-		if (!err) {
-			client.publish('presence', 'Hello mqtt')
-			setInterval(intervalFunc, 1500);
-	  }
-	})
-  })
+//    client.publish('presence', 'Hello mqtt')
+    setInterval(intervalFunc, 5000);
+})
--- TP2/vm2/subs/index.js
+++ TP2/vm2/subs/index.js
@@ -1,8 +1,19 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://' + process.env.IP + ':' + process.env.LISTEN);
+//var client  = mqtt.connect('mqtt://test.mosquitto.org')
+var client  = mqtt.connect('mqtt://brokertp2:1883')
+//var client  = mqtt.connect('mqtt://'+process.env.BROKERNAME)
+
+ 
+client.on('connect', function() {
+    client.subscribe('presence', function (err) {
+        if (!err) {
+            console.log('matenme! no literalmente');
+        }
+    })
+})
 
 client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
-})
+    // message is Buffer
+    console.log(message.toString())
+    //client.end()
+  })

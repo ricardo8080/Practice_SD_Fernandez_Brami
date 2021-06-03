2a0a6a0905bf738e6b2f5930a6434a436f5b0adf
Brami01 <bramopit@gmail.com>
2021-06-02 23:24:51 -0400

    dividing folders

---
+++ TP3/c/vm2/subscriber/index.js
@@ -0,0 +1,16 @@
+var mqtt = require('mqtt');
+var client  = mqtt.connect('mqtt://' + process.env.HOST + ':' + process.env.PORT);
+
+client.on('connect', function() {
+    client.subscribe(process.env.TOPIC, function (err) {
+        if (!err) {
+            console.log('connected');
+        }
+    })
+})
+
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString());
+    //client.end()
+  })

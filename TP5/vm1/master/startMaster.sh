be012ded5d7a9dc66bebdbf045bc0682b8aab746
Ricardo <ricardo.fernandez.bo.2000@ieee.org>
2021-06-12 17:15:16 -0400

    Prepared the wait-for implementation in Dockerfile, changed env vars to be in nab.env file and modified the docker-compose, copied publisher folder from TP4 as base for master

---
+++ TP5/vm1/master/startMaster.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "waiting for server ${SERVER} on port ${PORT} for ${TIMEOUT} seconds"
+
+#./wait-for ${SERVER}:${PORT} -t ${TIMEOUT} -- node --version
+./wait-for ${SERVER}:${PORT} -t ${TIMEOUT} -- node index.js
+
+echo "server ready"

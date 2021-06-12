52e9e293abf2e32b3ad382e44c2ace390a8083c3
Ricardo <ricardo.fernandez.bo.2000@ieee.org>
2021-06-12 17:25:16 -0400

    added worker based on master to docker-compose

--- TP5/master/startMaster.sh
+++ TP5/master/startMaster.sh
@@ -2,7 +2,6 @@
 
 echo "waiting for server ${SERVER} on port ${PORT} for ${TIMEOUT} seconds"
 
-#./wait-for ${SERVER}:${PORT} -t ${TIMEOUT} -- node --version
 ./wait-for ${SERVER}:${PORT} -t ${TIMEOUT} -- node index.js
 
 echo "server ready"
---
+++ TP5/worker/index.js
@@ -0,0 +1,37 @@
+const mqtt = require('mqtt');
+const ip = require("ip");
+const os = require("os");
+const process = require('process');
+//const client  = mqtt.connect('mqtt://'+ process.env.BROKERNAME+ ':'+ process.env.PORT );
+const client  = mqtt.connect('mqtt://'+ process.env.BROKERNAME);
+const id = os.hostname();
+
+function intervalFunc() {
+    client.publish(process.env.TOPIC, getData());
+    console.log("aaa")
+};
+
+function getData() {
+    const time = new Date();
+    var months = ["January", "February", "March", "April", "May", 
+    "June", "July", "August", "September", "October", "November", "December"];
+    var days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+    const dateFormated = days[time.getDay()]+ " " +
+                        months[time.getMonth()] + " "  +
+                        time.getDate() + " "  +
+                        time.getHours() + ":" +
+                        time.getMinutes() + ":" +
+                        time.getSeconds() +
+                        " -04 " + time.getFullYear();
+    const data = {
+        time: dateFormated,
+        container: id,
+        ip: ip.address()
+    }
+    return JSON.stringify(data);
+};
+
+
+client.on('connect', function () {
+    setInterval(intervalFunc, 5000);
+});

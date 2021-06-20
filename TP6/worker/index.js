f0a9f800622fbd1bdb0df4794dc36be075aa9c54
Brami01 <bramopit@gmail.com>
2021-06-19 22:46:22 -0400

    a little change

--- TP6/worker/index.js
+++ TP6/worker/index.js
@@ -66,6 +66,6 @@ client_grpc.register({message: getId()}, function(err, response) {
 
 var server_grpc = new grpc.Server();
 server_grpc.addService(hello_proto.Greeter.service, {sendTask: sendTask});
-server_grpc.bindAsync( id + ':9090', grpc.ServerCredentials.createInsecure(), () => {
+server_grpc.bindAsync(id + ':9090', grpc.ServerCredentials.createInsecure(), () => {
   server_mqtt.start();
 });
